35dcd2dd12f64b73c5f808dfff3d4a7e9a9bca90
ToKimSeoYeon <rlagmlwnd3@daum.net>
2019-03-31 00:12:28 +0900

    타임아웃 기능으로 Loading화면 구현

--- movieapp-react/src/App.js
+++ movieapp-react/src/App.js
@@ -8,26 +8,7 @@ class App extends Component {
 
   state = {
     greeting: 'Hello!',
-    movies: [
-      {
-        title: "Matrix",
-        poster: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg"
-      },
-      {
-        title: "Full Metal Jacket",
-        poster: "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg"
-      },
-      {
-        title: "Oldboy",
-        poster: "https://m.media-amazon.com/images/M/MV5BMTI3NTQyMzU5M15BMl5BanBnXkFtZTcwMTM2MjgyMQ@@._V1_UX182_CR0,0,182,268_AL_.jpg"
-      },
-      {
-        title: "Star Wars",
-        poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjQItr6vlitIx6I23CbA6PlfQh1h7nFrCvVNRRfeZhJUVHKLHvDA"
-      }
-      
     
-    ]
   }
 
   componentDidMount() {
@@ -35,23 +16,44 @@ class App extends Component {
       //infinite scroll(페이스북 형식)의 힌트
       this.setState({ 
         movies: [
-          ...this.state.movies, // 이전 영화리스트
           {
-            title: "Trainspotting",
-            poster: "https://g7w3q9i5.stackpathcdn.com/wp-content/uploads/2018/11/Trainspotting-NEW.jpg"
+            title: "Matrix",
+            poster: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg"
+          },
+          {
+            title: "Full Metal Jacket",
+            poster: "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg"
+          },
+          {
+            title: "Oldboy",
+            poster: "https://m.media-amazon.com/images/M/MV5BMTI3NTQyMzU5M15BMl5BanBnXkFtZTcwMTM2MjgyMQ@@._V1_UX182_CR0,0,182,268_AL_.jpg"
+          },
+          {
+            title: "Star Wars", 
+            poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjQItr6vlitIx6I23CbA6PlfQh1h7nFrCvVNRRfeZhJUVHKLHvDA"
           }
+          
+        
         ]
       })
-    }, 1000)
+    }, 2000)
+  }
+
+  // 영화리스트를 불러오는 function
+  // _ 를 쓰는 이유는 자체기능과 자체기능이 아닌것을 구분하기위해, 나의 기능은 _ 로 시작함
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title ={movie.title} poster={movie.poster} key={index} />
+    })
+    // 영화리스트들은 movies라는 한곳에 저장함
+    return movies
   }
 
   render() {
 
     return (
       <div className="App">
-        {this.state.movies.map((movie, index) => {
-          return <Movie title={movie.title} poster={movie.poster} key={index}/>
-        })}
+      {this.state.movies ? this._renderMovies() : 'Loading '}
       </div>
     );
   }
