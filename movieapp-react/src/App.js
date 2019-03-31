e33fc75e7e944e88aea69ec5a2141b6b4280cc7c
ToKimSeoYeon <rlagmlwnd3@daum.net>
2019-03-31 16:03:00 +0900

    apply aysnc & await

--- movieapp-react/src/App.js
+++ movieapp-react/src/App.js
@@ -6,26 +6,40 @@ import Movie from './movie';
 
 class App extends Component {
 
-  state = {
-    
-  }
+  state = {}
 
   componentDidMount() {
-    fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
-    // .then(response => console.log(response))
-    .then(potato => potato.json()) // json으로 바꿔주기
-    .then(json => console.log(json))
-    .catch(err => console.log(err))
+    this._getMovies();
   }
 
   // 영화리스트를 불러오는 function
   // _ 를 쓰는 이유는 자체기능과 자체기능이 아닌것을 구분하기위해, 나의 기능은 _ 로 시작함
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title ={movie.title} poster={movie.poster} key={index} />
+    const movies = this.state.movies.map(movie => {
+      return <Movie title ={movie.title} poster={movie.large_cover_image} key={movie.id} />
     })
     // 영화리스트들은 movies라는 한곳에 저장함
     return movies
+  }
+
+  _getMovies = async () => {
+    const movies =  await this._callApi()
+    this.setState({
+      movies
+    })
+    
+  }
+  
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    // .then(response => console.log(response))
+    .then(potato => potato.json()) // json으로 바꿔주기
+    .then(json => json.data.movies)
+    .then(    setTimeout((() => {
+      console.log("callApi하는중")
+    }),10000))
+    .catch(err => console.log(err))
+    
   }
 
   render() {
